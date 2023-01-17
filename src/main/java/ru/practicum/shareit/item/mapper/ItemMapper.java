8dae5eb2a09e0ed5ac4c20f5bb08a6896a035474
Anton <antnbrnv@yandex.ru>
2023-01-17 12:13:22 +0300

    style: fix style

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.item.mapper;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.mapstruct.Mapper;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
@@ -11,24 +9,3 @@ public interface ItemMapper {
     ItemDto toItemDto(Item item);
     Item toItem(ItemDto itemDto, long userId);
 }
-
-/*@Getter
-@Setter
-public class ItemMapper {
-
-    public static ItemDto toItemDto(Item item) {
-        return new ItemDto(item.getId(),
-                item.getName(),
-                item.getDescription(),
-                item.getAvailable()
-        );
-    }
-
-    public static Item toItem(ItemDto itemDto, long userId) {
-        return new Item(itemDto.getId(),
-                itemDto.getName(),
-                itemDto.getDescription(),
-                itemDto.getAvailable(),
-                userId);
-    }
-}*/
--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -4,14 +4,6 @@ import org.mapstruct.Mapper;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
-
-/*public class UserMapper {
-    public static User toUser(UserDto userDto) {
-        return new User(userDto.getId(),
-                userDto.getName(),
-                userDto.getEmail());
-    }
-}*/
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User toUser(UserDto userDto);
