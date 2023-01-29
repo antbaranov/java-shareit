f387020221dddb87e5b2bd6445c5832848932e6f
Anton <2639231@gmail.com>
2023-01-29 15:20:14 +0300

    fix: UserMapper

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -11,12 +11,15 @@ import ru.practicum.shareit.user.model.User;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserInfoDto toUserInfoDto(User user);
-
     UserDto toUserDto(User user);
-
     User toUser(UserDto userDto);
-
-    User matchUser(UserDto userDto, User user);
+    static User matchUser(UserDto userDto, User user) {
+        return User.builder()
+                .id(userDto.getId())
+                .name(userDto.getName() == null ? user.getName() : userDto.getName())
+                .email(userDto.getEmail() == null ? user.getEmail() : userDto.getEmail())
+                .build();
+    }
 }
 
 
