1615dadd5d0dd0c05037021358f9b1164dfcea98
Anton <2639231@gmail.com>
2023-02-12 11:06:40 +0300

    test: add exception test

--- src/test/java/ru/practicum/shareit/booking/exception/InvalidDateTimeExceptionTest.java
+++ src/test/java/ru/practicum/shareit/booking/exception/InvalidDateTimeExceptionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 class InvalidDateTimeExceptionTest {
 
     @Test
-    void nvalidDateTimeExceptionTest() {
+    void invalidDateTimeExceptionTest() {
         InvalidDateTimeException invalidDateTimeException = new InvalidDateTimeException("test");
     }
 }
