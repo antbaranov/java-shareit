9a81c2f2d71b788b33ed7fad9e55ad69f84520e2
Anton <2639231@gmail.com>
2023-02-12 10:56:24 +0300

    test: add exception test

--- src/test/java/ru/practicum/shareit/booking/exception/BookingNotFoundExceptionTest.java
+++ src/test/java/ru/practicum/shareit/booking/exception/BookingNotFoundExceptionTest.java
@@ -1,9 +1,6 @@
 package ru.practicum.shareit.booking.exception;
 
 import org.junit.jupiter.api.Test;
-import ru.practicum.shareit.exception.ObjectNotFoundException;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class BookingNotFoundExceptionTest {
 
