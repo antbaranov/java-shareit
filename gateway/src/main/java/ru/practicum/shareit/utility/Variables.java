e3204feb728add7c6bc9a2910af81fd8dc23714f
Anton <2639231@gmail.com>
2023-02-26 20:39:30 +0300

    fix: подключение к БД 14

---
+++ gateway/src/main/java/ru/practicum/shareit/utility/Variables.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.utility;
+
+public class Variables {
+    public static final String userIdHeader = "X-Sharer-User-Id";
+    private static final String API_PREFIX = "/bookings";
+
+}
