969ce923eef629b101ab9feab7119df5cb7d3ddd
Anton <antnbrnv@yandex.ru>
2023-02-28 13:23:31 +0300

    fix: docker-compose.yml удалить SPRING_DATASOURCE_URL=jdbc:postgresql://db:5432/shareit

--- server/shareit-server-business/src/main/java/ru/practicum/shareit/user/model/User.java
+++ server/shareit-server-business/src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.user.model;
 
-import lombok.*;
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
